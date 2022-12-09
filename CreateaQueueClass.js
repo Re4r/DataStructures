8a314f6f8f9caafacdce963b58ef1fe60d4eaaa9
KirillMar <re4r@bk.ru>
2022-12-09 19:21:18 +0300

    IV

---
+++ CreateaQueueClass.js
@@ -0,0 +1,42 @@
+class Queue {
+    constructor(...item) {
+        this.collection = item;
+        this.start = 0;
+        this.end = 0;
+    }
+
+    print() {
+        console.log(this.collection);
+    }
+
+    enqueue(value) {
+        this.collection[this.end++] = value;
+    }
+
+    dequeue() {
+        return this.collection[this.start++];
+    }
+
+    front() {
+        return this.collection[this.start];
+    }
+
+    size() {
+        return this.end - this.start;
+    }
+
+    isEmpty() {
+        return this.size() === 0;
+    }
+}
+
+const newQueue = new Queue(4,6,7,8,9);
+
+newQueue.print();
+newQueue.enqueue(9978);
+newQueue.enqueue(777);
+console.log(newQueue.dequeue());
+console.log(newQueue.size());
+console.log(newQueue.front());
+console.log(newQueue.isEmpty());
+newQueue.print();
