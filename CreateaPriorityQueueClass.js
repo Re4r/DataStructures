243675fc227366f1d1d77ad36727e5edb78a2c3b
KirillMar <re4r@bk.ru>
2022-12-10 21:54:09 +0300

    V

---
+++ CreateaPriorityQueueClass.js
@@ -0,0 +1,40 @@
+function PriorityQueue(...elem) {
+    this.collection = elem;
+    this.printCollection = function() {
+        console.log(this.collection);
+    };
+
+this.enqueue = function(item) {
+    let index = this.collection.findIndex(elem => elem[1] > item[1]);
+    if (index !== -1) {
+        this.collection.splice(index, 0, item);
+    } else {
+        this.collection.push(item);
+    }
+}
+
+this.dequeue = function() {
+    return this.collection.shift()[0];
+}
+
+this.size = function() {
+    return this.collection.length;
+}
+
+this.front = function() {
+    return this.collection[0][0];
+}
+
+this.isEmpty = function() {
+    return this.size === 0;
+}
+
+}
+
+const prqe = new PriorityQueue(['kitten', 2], ['dog', 2], ['rabbit', 2]);
+
+console.log(prqe.printCollection());
+prqe.enqueue([ 'human', 1 ]);
+console.log(prqe.printCollection());
+prqe.enqueue([ 'fox', 3 ]);
+console.log(prqe.printCollection());
