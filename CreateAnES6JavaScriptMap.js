6c124f1859a733e0f52e5225423e4d1960d0d0ec
KirillMar <re4r@bk.ru>
2022-12-12 20:49:34 +0300

    XVII

---
+++ CreateAnES6JavaScriptMap.js
@@ -0,0 +1,10 @@
+const myMap = new Map();
+
+myMap.set("freeCodeCamp", "Awesome");
+myMap.set("lotus", "flower");
+myMap.set("bird", "parrot");
+
+console.log(myMap.keys());
+console.log(myMap.values());
+console.log(myMap.entries());
+console.log(myMap);
