284a01989efecb415b87e863c4dcb2cb8c019105
KirillMar <re4r@bk.ru>
2022-12-15 20:09:43 +0300

    XXXVI

---
+++ DeleteNodeTwoChildrenBST.js
@@ -0,0 +1,91 @@
+const displayTree = (tree) => {
+    console.log(JSON.stringify(tree,null,2));
+}
+
+const Node = function(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+};
+
+function BinarySearchTree() {
+    this.root = null;
+
+    this.add = (value) => {
+        let current = this.root;
+        const node = new Node(value);
+        let direction = '';
+
+        if (!current) {
+            this.root = node;
+            return undefined;
+        }
+
+        while (current) {
+            if (current.value === value) {
+                return null;
+            }
+
+            direction = (value < current.value) ? 'left' : 'right';
+
+            if (!current[direction]) {
+                current[direction] = node;
+                return undefined;
+            }
+            current = current[direction];
+        }
+    };
+
+    this.remove = (value) => {
+        if (!this.root) return null;
+
+        let parent;
+        let target = this.root;
+        while (target && target.value !== value) {
+            parent = target;
+            if (target.value > target) {
+                target = target.left;
+            } else {
+                target = target.right;
+            }
+        }
+        if (!target) return null;
+
+        if (!(target.left && target.right)) {
+
+        const replacement = target.right ? target.right : target.left;
+        if (!parent) {
+            this.root = replacement;
+        } else {
+            const direction = parent.left === target ? 'left' : 'right';
+            parent[direction] = replacement;
+        }
+
+        } else {
+            const newChildValue = this.findMin(target.right);
+            this.remove(newChildValue);
+            target.value = newChildValue;
+        }  
+    };
+
+    this.findMin = (node = this.root) => {
+        if (!node) return null;
+        return node.left ? this.findMin(node.left) : node.value;
+    };
+
+    this.findMax = (node = this.root) => {
+        if (!node) return null;
+        return node.right ? this.findMax(node.right) : node.value;
+    };
+}
+
+const newBin = new BinarySearchTree();
+newBin.add(33);
+newBin.add(22);
+newBin.add(44);
+newBin.add(20);
+newBin.add(23);
+newBin.add(45);
+newBin.add(38);
+newBin.remove(44);
+displayTree(newBin);
